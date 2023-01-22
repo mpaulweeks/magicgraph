9c12b2352f8c9f47fd0b12abdc58ec3806cde9b0
mpaulweeks <mpaulweeks@gmail.com>
2023-01-22 12:40:31 -0500

    choose deck

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,30 @@ import { DeckView } from './app/DeckView';
 import { Deck } from './lib/deck';
 
 function App() {
-  const deck = new Deck('Larry Loops', LarryDraft);
+  const queryParsm = new URLSearchParams(window.location.search);
+
+  let deck: Deck | undefined;
+
+  if (queryParsm.get('larry')) {
+    deck = new Deck('Larry Loops', LarryDraft);
+  }
+
+  if (deck) {
+    return (
+      <DeckView deck={deck} />
+    );
+  }
+
   return (
-    <DeckView deck={deck} />
-  );
+    <div>
+      <h1>Choose deck</h1>
+      <div>
+        <button onClick={() => window.location.href = './?larry=1'}>
+          Larry Loops
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
