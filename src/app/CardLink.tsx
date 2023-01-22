c795440e0cdb8bc90d6b9adaf35b8f216732b483
mpaulweeks <mpaulweeks@gmail.com>
2023-01-22 13:06:37 -0500

    autocard in edges, add >> for anchor links

--- src/app/CardLink.tsx
+++ src/app/CardLink.tsx
@@ -1,5 +1,19 @@
 import { Cardlike } from "../types"
+import { AutoCard } from "./AutoCard";
+import styled from 'styled-components';
+
+const CardLinkContainer = styled.span`
+& a {
+  text-decoration: none;
+}
+`;
 
 export const CardLink = (props: {
   card: Cardlike;
-}) => <a href={'#' + props.card.id}>{props.card.name}</a>;
+}) => (
+  <CardLinkContainer>
+    <a href={'#' + props.card.id}>{'>>'}</a>
+    {' '}
+    <AutoCard card={props.card} />
+  </CardLinkContainer>
+);
