18070386c17f2c3caae94ef32bcac1a8213ac0b1
mpaulweeks <mpaulweeks@gmail.com>
2023-01-31 10:59:24 -0500

    remove unused old graph cpde

--- src/lib/deck.ts
+++ src/lib/deck.ts
@@ -2,7 +2,6 @@ import { CardDraft, CardEdge } from "../types";
 import { sortBy } from "../util/list";
 import { CardImpl } from "./card";
 import { Graph } from "./graph";
-import { IdMapper } from "./idmapper";
 
 export class Deck {
   readonly cards: CardImpl[];
@@ -14,21 +13,5 @@ export class Deck {
   ) {
     this.cards = sortBy(drafts.map(d => new CardImpl(d)), c => c.name);
     this.edges = new Graph().getEdges(this.cards);
-  }
-
-  getGraph() {
-    const mapper = new IdMapper<string>();
-    const graph = {
-      nodes: this.cards.map((c, ci) => ({
-        id: mapper.id(c.id),
-        label: c.name,
-        title: c.name,
-      })),
-      edges: this.edges.map(e => ({
-        from: mapper.id(e.related[0].id),
-        to: mapper.id(e.related[1].id),
-      })),
-    };
-    return graph;
   }
 }
