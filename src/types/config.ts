8e695b27f202768ff3672673809d8d605eb8d301
mpaulweeks <mpaulweeks@gmail.com>
2023-04-25 09:48:29 -0400

    split up types

---
+++ src/types/config.ts
@@ -0,0 +1,56 @@
+import { Interset } from '../util/interset';
+import { CardListStatus } from './enum';
+
+export interface Cardlike {
+  readonly id: string;
+  readonly name: string;
+  readonly nick: string;
+  readonly types: Interset<string>;
+  readonly subtypes: Interset<string>;
+  readonly mc: string;
+  readonly mv: number;
+  readonly category: string;
+  readonly tags: Interset<string>;
+  readonly notes: string[];
+  readonly combos: CardCombo[];
+  readonly pending: boolean;
+  readonly rejected: boolean;
+}
+
+export interface CardCombo {
+  edgeType: string;
+  match: (other: Cardlike) => boolean;
+}
+
+export interface Matcher {
+  relationship: string;
+  isMatch(a: Cardlike, b: Cardlike): boolean;
+}
+
+export interface CardDraft {
+  status?: CardListStatus;
+  name: string;
+  nick?: string;
+  types: string[];
+  subtypes?: string[];
+  mc?: string;
+  category: string;
+  tags?: string[];
+  notes?: string[];
+  combos?: CardCombo[];
+}
+
+export interface CardEdge {
+  relationship: string;
+  related: Cardlike[];
+}
+
+export type DeckData = {
+  name: string;
+  url?: string;
+  cardDrafts: CardDraft[];
+  matchers: Matcher[];
+  categoryColorMap: Record<string, string>;
+  relationshipInverse: Record<string, string>;
+  relationshipOrder: string[];
+};
---
+++ src/types/enum.ts
@@ -0,0 +1,17 @@
+export enum CardListStatus {
+  Default = 1,
+  Pending,
+  Rejected,
+}
+
+export enum CardType {
+  Land = 'Land',
+  Creature = 'Creature',
+  Artifact = 'Artifact',
+  Equipment = 'Equipment',
+  Enchantment = 'Enchantment',
+  Aura = 'Aura',
+  Instant = 'Instant',
+  Sorcery = 'Sorcery',
+  Planeswalker = 'Planeswalker',
+}
